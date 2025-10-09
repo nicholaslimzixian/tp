2ef4725f747f4d3004d90c56aac251ca57295084
Zirun <hhvrfn@outlook.com>
2025-10-10 00:15:33 +0800

    Fix CsvUtil to support Favorite field in Person constructor

--- src/main/java/seedu/address/commons/util/CsvUtil.java
+++ src/main/java/seedu/address/commons/util/CsvUtil.java
@@ -18,6 +18,7 @@ import com.opencsv.exceptions.CsvValidationException;
 import seedu.address.model.faculty.Faculty;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.Favorite;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
@@ -57,7 +58,8 @@ public class CsvUtil {
                 Set<Tag> allTags = parseTags(nextLine[4]);
                 Set<Faculty> allFaculties = parseFaculties(nextLine[5]);
 
-                Person newPerson = new Person(name, phoneNo, email, address, allTags, allFaculties);
+                Person newPerson = new Person(name, phoneNo, email, address, allTags, allFaculties,
+                        Favorite.DEFAULT_NOT_FAVORITE);
 
                 contacts.add(newPerson);
             }
