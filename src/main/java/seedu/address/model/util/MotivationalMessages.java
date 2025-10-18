a415fd609239c1d6a6ad562292fc46f60d7282ea
zuo yi <e1481832@u.nus.edu>
2025-10-19 00:14:37 +0800

    Add motivational message

--- src/main/java/seedu/address/model/util/MotivationalMessages.java
+++ src/main/java/seedu/address/model/util/MotivationalMessages.java
@@ -21,7 +21,6 @@ public class MotivationalMessages {
             "Great things never come from comfort zones.",
             "Success doesn’t just find you. You have to go out and get it.",
             "The only way to do great work is to love what you do. - Steve Jobs",
-            "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill",
             "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
             "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
             "You are never too old to set another goal or to dream a new dream. - C.S. Lewis"
